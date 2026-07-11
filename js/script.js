34c08dc274282a1c39ae50c3aa211b47585e61f6
nonoka <it262155@trident.ac.jp>
2026-07-12 05:07:15 +0900

    Moonflow Yoga Studioという架空のヨガ教室のWebサイト

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// トップページのスライド
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
